e0985f4b6692bf43213d288b18dc5e5dac43859b
gencdevops <gencdevops@gmail.com>
2021-08-19 11:23:03 +0300

    Initial commit

---
+++ CreateObjectAutoWithComponent/src/main/java/com/example/createobjectautowithcomponent/CreateObjectAutoWithComponentApplication.java
@@ -0,0 +1,16 @@
+package com.example.createobjectautowithcomponent;
+
+import org.csystem.util.Console;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateObjectAutoWithComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateObjectAutoWithComponentApplication.class, args);
+
+
+    }
+
+}
---
+++ CreateObjectAutoWithComponent/src/test/java/com/example/createobjectautowithcomponent/CreateObjectAutoWithComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createobjectautowithcomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateObjectAutoWithComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
