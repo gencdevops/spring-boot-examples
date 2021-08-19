4b9ae36adb2a8b41f0bce52fa16c18ef69ae81a3
gencdevops <gencdevops@gmail.com>
2021-08-19 12:20:11 +0300

    Component and PostConstruct annotation example

--- CreateObjectAutoWithComponent/src/main/java/com/example/createobjectautowithcomponent/component/DateAndTimeDisplay.java
+++ CreateObjectAutoWithComponent/src/main/java/com/example/createobjectautowithcomponent/component/DateAndTimeDisplay.java
@@ -1,7 +1,35 @@
 package com.example.createobjectautowithcomponent.component;
 
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
 public class DateAndTimeDisplay {
     public DateAndTimeDisplay() {
         System.out.println("DateAndTimeDisplay . DateAndTimeDisplay() ");
     }
+
+    @PostConstruct
+    public void DisplayTime() {
+        LocalTime now = LocalTime.now();
+
+        String nowText = DateTimeFormatter.ofPattern("hh:mm:ss").format(now);
+
+        System.out.println("DateAndTimeDisplay.DisplayTime : " + nowText);
+    }
+
+    @PostConstruct
+    public void DisplayDate() {
+        LocalDate today = LocalDate.now();
+
+        String nowText = DateTimeFormatter.ofPattern("dd:MM:yyyy").format(today);
+
+        System.out.println("DateAndTimeDisplay.DisplayDate : " + nowText);
+    }
+
 }
--- CreateObjectAutoWithComponent/src/main/java/com/example/createobjectautowithcomponent/component/DateTimeDisplay.java
+++ CreateObjectAutoWithComponent/src/main/java/com/example/createobjectautowithcomponent/component/DateTimeDisplay.java
@@ -1,7 +1,28 @@
 package com.example.createobjectautowithcomponent.component;
 
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
 public class DateTimeDisplay {
     public DateTimeDisplay() {
         System.out.println("DateTimeDisplay. DateTimeDisplay() ");
     }
+
+    @PostConstruct
+    public void displayDateTime() {
+        LocalDateTime now = LocalDateTime.now();
+
+        String nowText = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss").format(now);
+
+        System.out.println("DateTimeDisplay : " + nowText);
+    }
+
+
+
+
 }
