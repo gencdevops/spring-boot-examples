b117fe8aa0420a3c1a96fdfcc866fa7006aebb2f
gencdevops <gencdevops@gmail.com>
2021-08-26 14:40:18 +0300

    application.properties example

---
+++ ValueAnnotation/src/main/java/com/example/valueannotation/component/SecondOperation.java
@@ -0,0 +1,21 @@
+package com.example.valueannotation.component;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class SecondOperation {
+    @Value("${operation.second.operand1:80}")
+    private int a;
+    @Value("${operation.second.operand2}")
+    private int b;
+    @Value("${operation.second.operator}")
+    private String prefix;
+
+    @PostConstruct
+    public void doOperation() {
+        System.out.printf("%s:%d + %d = %d%n", prefix, a, b, a + b);
+    }
+}
