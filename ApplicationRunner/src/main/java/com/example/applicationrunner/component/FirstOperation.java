7698deaba047480a07bf67af8dd71469ec139913
gencdevops <gencdevops@gmail.com>
2021-08-26 14:55:17 +0300

    application.runner initial

---
+++ ApplicationRunner/src/main/java/com/example/applicationrunner/component/FirstOperation.java
@@ -0,0 +1,23 @@
+package com.example.applicationrunner.component;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class FirstOperation {
+    @Value("${operation.first.operand1}")
+    private int a;
+    @Value("${operation.first.operand2}")
+    private int b;
+    @Value("${operation.first.operator}")
+    private String prefix;
+
+    @PostConstruct
+    public void doOperation() {
+        System.out.printf("%s:%d + %d = %d%n", prefix, a, b, a + b);
+    }
+
+}
