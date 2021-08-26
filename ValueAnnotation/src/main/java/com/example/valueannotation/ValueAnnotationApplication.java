3626b6413e6e8fcfc834e5384e7f6a5e9e03365d
gencdevops <gencdevops@gmail.com>
2021-08-26 12:33:29 +0300

    ValueAnnotation initial

---
+++ ValueAnnotation/src/main/java/com/example/valueannotation/ValueAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.valueannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValueAnnotationApplication.class, args);
+    }
+
+}
---
+++ ValueAnnotation/src/test/java/com/example/valueannotation/ValueAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.valueannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
