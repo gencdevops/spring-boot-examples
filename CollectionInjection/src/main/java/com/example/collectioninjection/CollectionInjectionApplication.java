3f284e8d3587953f745bd728b7817ba3e0747ee3
gencdevops <gencdevops@gmail.com>
2021-08-27 20:13:51 +0300

    CollectionInjectionApplication initial

---
+++ CollectionInjection/src/main/java/com/example/collectioninjection/CollectionInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectioninjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionInjectionApplication.class, args);
+    }
+
+}
---
+++ CollectionInjection/src/test/java/com/example/collectioninjection/CollectionInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collectioninjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
