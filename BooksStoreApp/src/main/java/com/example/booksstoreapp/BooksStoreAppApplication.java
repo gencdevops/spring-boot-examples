7663586f4847fb41675ef7fffa39a71461e1f8f6
gencdevops <gencdevops@gmail.com>
2021-09-13 15:30:59 +0300

    BookStoreApp anitialize

---
+++ BooksStoreApp/src/main/java/com/example/booksstoreapp/BooksStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksStoreAppApplication.class, args);
+    }
+
+}
---
+++ BooksStoreApp/src/test/java/com/example/booksstoreapp/BooksStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
