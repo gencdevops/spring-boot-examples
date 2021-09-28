a43dfde638d4eca66c4654bd64e1fa5062ac86a9
gencdevops <gencdevops@gmail.com>
2021-09-28 19:02:16 +0300

    DirectoryService added functional exception

--- MovieApi/src/main/java/com/example/movieapi/service/DirectoryService.java
+++ MovieApi/src/main/java/com/example/movieapi/service/DirectoryService.java
@@ -3,6 +3,7 @@ package com.example.movieapi.service;
 import com.example.movieapi.converter.DirectorConverter;
 import com.example.movieapi.data.dataaccesslayer.MovieAppDAL;
 import com.example.movieapi.dto.DirectorDTO;
+import org.csystem.util.data.DatabaseUtil;
 import org.csystem.util.data.repository.RepositoryException;
 import org.csystem.util.data.service.DataServiceException;
 import org.springframework.stereotype.Service;
@@ -17,27 +18,29 @@ public class DirectoryService {
     private final MovieAppDAL movieAppDAL;
     private final DirectorConverter directorConverter;
 
+    private DirectorDTO saveDirectorCallback(DirectorDTO directorDTO) {
+        movieAppDAL.saveDirector(directorConverter.toDirector(directorDTO));
+        return directorDTO;
+    }
+
+    private List<DirectorDTO> findAllDirectorsCallback() {
+        return StreamSupport.stream(movieAppDAL.findAllDirectors().spliterator(), false)
+                .map(directorConverter::toDirectorDTO)
+                .collect(Collectors.toList());
+    }
+
     public DirectoryService(MovieAppDAL movieAppDAL, DirectorConverter directorConverter) {
         this.movieAppDAL = movieAppDAL;
         this.directorConverter = directorConverter;
     }
 
     public DirectorDTO saveDirector(DirectorDTO directorDTO) {
-        try {
-            movieAppDAL.saveDirector(directorConverter.toDirector(directorDTO));
-            return directorDTO;
-        } catch (RepositoryException ex) {
-            throw new DataServiceException("MovieAppService.saveDirector", ex.getCause());
-        }
+        return DatabaseUtil.doWorkForService(() -> saveDirectorCallback(directorDTO),
+                "MovieAppService.saveDirector");
     }
 
     public List<DirectorDTO> findAllDirectors() {
-        try {
-            return StreamSupport.stream(movieAppDAL.findAllDirectors().spliterator(), false)
-                    .map(directorConverter::toDirectorDTO)
-                    .collect(Collectors.toList());
-        } catch (RepositoryException ex) {
-            throw new DataServiceException("MovieAppService.findAllDirectors", ex.getCause());
-        }
+        return DatabaseUtil.doWorkForService(this::findAllDirectorsCallback,
+                "MovieAppService.findAllDirectors");
     }
 }
